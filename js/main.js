08070125f4338abda92ac4ff10fe5231ddf52b43
Natalja <tasia@inbox.lt>
2023-06-19 18:32:31 +0300

    js and html modify

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const buttomDOM = document.getElementById('mygtukas');
+const titleDOM = document.getElementById('antraste');
+
+function kaDaryti() {
+    titleDOM.textContent = 'Labas vakaras!';
+}
+
+buttomDOM.addEventListener('click', kaDaryti);
